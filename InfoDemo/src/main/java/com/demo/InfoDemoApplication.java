fb93e1abb886df470d410645e218dbfcb789c197
techopengroup <130648038+techopengroup@users.noreply.github.com>
2023-05-19 10:37:06 +0530

    Add files via upload

---
+++ InfoDemo/src/main/java/com/demo/InfoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoDemoApplication.class, args);
+	}
+
+}
